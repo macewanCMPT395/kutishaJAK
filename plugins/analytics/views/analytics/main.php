f07c3dc795946168c4dc5a0d5089754f955f47ce
Kari Bronson <bronsonk2@mymacewan.ca>
2014-03-26 03:20:43 -0400

    Pull Data using Json, Prepare Data Structure for Pie Chart

--- plugins/analytics/views/analytics/main.php
+++ plugins/analytics/views/analytics/main.php
@@ -42,12 +42,12 @@ label: "Series" + (i + 1),
     }
 }*/
         var data = [     
-                  { label: "Series1",  data: [[3,60]]},
-                  { label: "Series2",  data: [[1,30]]},
-                  { label: "Series3",  data: [[1,90]]},
-                  { label: "Series4",  data: [[1,70]]},
-                  { label: "Series5",  data: [[210,220]]},
-                  { label: "Series6",  data: [[20,0]]}
+                  { label: "Series1",  data: 60},
+                  { label: "Series2",  data: 30},
+                  { label: "Series3",  data: 90},
+                  { label: "Series4",  data: 70},
+                  { label: "Series5",  data: 220},
+                  { label: "Series6",  data: 0}
                 ];
 
 var pieholder = $("#pieholder");
@@ -78,13 +78,57 @@ legend: {
 }
 });
 });
+
+$.getJSON('http://10.0.2.15/~jharvard/kutishaJAK/index.php/api?task=version', function(data){
+    $("#footer").prepend(data['payload']['version'][0]['version'] + "  ");
+    });
+
+$.getJSON('http://10.0.2.15/~jharvard/kutishaJAK/index.php/api?task=incidents&by=catid&id=2', function(data){
+  var dataSet = [];
+    var dataLen = data['payload']['incidents'][0]['categories'][0]['category']['title']+ "---";
+    $("#footer").prepend(dataLen);
+    
+    for (var i=0; i<data['payload']['incidents'].length; i++){
+        for (var j=0; j<data['payload']['incidents'][i]['categories'].length; j++){
+       var title = data['payload']['incidents'][i]['categories'][j]['category']['title'];
+        var index = -1;
+        index = dataSet.map(function(e) { return e.label}).indexOf(title);
+        
+        if(index === -1){   
+            dataSet.push({ label: title, data: 1 });
+        }else {
+            console.log(dataSet[index]);
+        };
+
+       };
+    };
+
+   console.log(dataSet);
+
+    /*$.each(data['payload']['incidents'], function(incidents)    {
+       // debugger;
+        console.log(incidents);
+      
+      incidents['categories'].foreach( function (category){
+        console.log(category['title']);
+      });*/
+      
+      //  console.log(incidents['categories']);
+      //  console.log(incidents['categories']['category']['title']);
+      //  $.each(incidents['categories'], function(category) {
+      //     console.log(category.title);
+      //  });
+    // });
+    });
+
+
 function labelFormatter(label, series) {
         return "<div style='font-size:8pt; text-align:center; padding:2px; color:white;'>" + label + "<br/>" + Math.round(series.percent) + "%</div>";
-            }
-        </script>
-        stuff
+}
+</script>
 
 
+<?php echo "hello World"; ?>
 
 </div>
 </div>
