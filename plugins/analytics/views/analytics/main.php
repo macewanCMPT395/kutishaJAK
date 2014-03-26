602a6a2bee621b7cc60680fe155dd773ef85597d
Kari Bronson <bronsonk2@mymacewan.ca>
2014-03-26 04:25:52 -0400

    Full integration of data into Pie Chart, Format pie chart nicely with data

--- plugins/analytics/views/analytics/main.php
+++ plugins/analytics/views/analytics/main.php
@@ -2,6 +2,11 @@
 <div class = "content-bg">
 <div id="placeholder" style="width:600px;height:300px"></div>
 <div id="pieholder" style="width:600px;height:300px"></div>
+<style type="text/css">
+#description {
+        margin: 15px 10px 20px 10px;
+            }
+</style>
 <!--[if lte IE 8]><script language="javascript" type="text/javascript" src="../../excanvas.min.js"></script><![endif]-->
 <script language="javascript" type="text/javascript" src="../plugins/analytics/libraries/flot/jquery.js"></script>
 <script language="javascript" type="text/javascript" src="../plugins/analytics/libraries/flot/jquery.flot.js"></script>
@@ -29,42 +34,42 @@ $(function() {
         $("#footer").prepend("Flot " + $.plot.version + " &ndash; ");
         });
 
-//Pie Chart 
+//Array for pie chart
+var dataSet = [];
 
-/*//Generate Random Data
-var data = [],
-    series = Math.floor(Math.random() * 6) + 3;
 
-for (var i = 0; i < series; i++) {
-    data[i] = {
-label: "Series" + (i + 1),
-       data: Math.floor(Math.random() * 100) + 1
-    }
-}*/
-        var data = [     
-                  { label: "Series1",  data: 60},
-                  { label: "Series2",  data: 30},
-                  { label: "Series3",  data: 90},
-                  { label: "Series4",  data: 70},
-                  { label: "Series5",  data: 220},
-                  { label: "Series6",  data: 0}
-                ];
+$.getJSON('http://10.0.2.15/~jharvard/kutishaJAK/index.php/api?task=incidents', function(data){
 
+    //Incidents is an array of incidents objects
+    for (var i=0; i<data['payload']['incidents'].length; i++){
+        //Categories is an array of Category objects
+        for (var j=0; j<data['payload']['incidents'][i]['categories'].length; j++){
+           //Each Incident has multiple Categorical Names
+           var title = data['payload']['incidents'][i]['categories'][j]['category']['title'];
+            var index = -1;
+            index = dataSet.map(function(e) { return e.label}).indexOf(title);
+            
+            if(index === -1){   
+                dataSet.push({ label: title, data: 1 });
+            }else {
+                dataSet[index]['data']++;
+            };
+        };
+    };
+
+//Pie Chart Code
 var pieholder = $("#pieholder");
 
 $(function() {
 
-        pieholder.unbind();
-
-        $("#title").text("Default pie chart");
-        $("#description").text("The default pie chart with no options set.");
-
-        $.plot(pieholder, data, {
+pieholder.unbind();
+$.plot(pieholder, dataSet, {
 series: {
 pie: { 
 show: true,
 radius: 1,
 label: {
+threshold: 0.04,
 show: true,
 formatter: labelFormatter,
 background: {
@@ -79,56 +84,12 @@ legend: {
 });
 });
 
-$.getJSON('http://10.0.2.15/~jharvard/kutishaJAK/index.php/api?task=version', function(data){
-    $("#footer").prepend(data['payload']['version'][0]['version'] + "  ");
-    });
-
-$.getJSON('http://10.0.2.15/~jharvard/kutishaJAK/index.php/api?task=incidents&by=catid&id=2', function(data){
-  var dataSet = [];
-    var dataLen = data['payload']['incidents'][0]['categories'][0]['category']['title']+ "---";
-    $("#footer").prepend(dataLen);
-    
-    for (var i=0; i<data['payload']['incidents'].length; i++){
-        for (var j=0; j<data['payload']['incidents'][i]['categories'].length; j++){
-       var title = data['payload']['incidents'][i]['categories'][j]['category']['title'];
-        var index = -1;
-        index = dataSet.map(function(e) { return e.label}).indexOf(title);
-        
-        if(index === -1){   
-            dataSet.push({ label: title, data: 1 });
-        }else {
-            console.log(dataSet[index]);
-        };
-
-       };
-    };
-
-   console.log(dataSet);
-
-    /*$.each(data['payload']['incidents'], function(incidents)    {
-       // debugger;
-        console.log(incidents);
-      
-      incidents['categories'].foreach( function (category){
-        console.log(category['title']);
-      });*/
-      
-      //  console.log(incidents['categories']);
-      //  console.log(incidents['categories']['category']['title']);
-      //  $.each(incidents['categories'], function(category) {
-      //     console.log(category.title);
-      //  });
-    // });
-    });
+});
 
 
 function labelFormatter(label, series) {
         return "<div style='font-size:8pt; text-align:center; padding:2px; color:white;'>" + label + "<br/>" + Math.round(series.percent) + "%</div>";
 }
 </script>
-
-
-<?php echo "hello World"; ?>
-
 </div>
 </div>
